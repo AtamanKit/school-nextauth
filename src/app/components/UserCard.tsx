7c59870b89a3ca19275a6bacbb33c6ae732027cf
AtamanKit <rodionatamaniuc@gmail.com>
2023-11-07 18:16:16 +0200

    Downgraded next version to be compatible with styles

--- src/app/components/UserCard.tsx
+++ src/app/components/UserCard.tsx
@@ -12,15 +12,24 @@ type Props = {
 }
 
 export default function Card({ user, pagetype }: Props) {
+
+    //console.log(user)
+
     const greeting = user?.name ? (
         <div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
-            Hello {user?.name}
+            Hello {user?.name}!
         </div>
     ) : null
 
+    // const emailDisplay = user?.email ? (
+    //     <div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
+    //         {user?.email}
+    //     </div>
+    // ) : null
+
     const userImage = user?.image ? (
         <Image
-            className="border-4 border-black dark:border-state-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+            className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
             src={user?.image}
             width={200}
             height={200}
@@ -32,6 +41,7 @@ export default function Card({ user, pagetype }: Props) {
     return (
         <section className="flex flex-col gap-4">
             {greeting}
+            {/* {emailDisplay} */}
             {userImage}
             <p className="text-2xl text-center">{pagetype} Page!</p>
         </section>
